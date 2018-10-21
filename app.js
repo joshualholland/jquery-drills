3a0a8284854a06aaf495f2c72e8904e6f0e90904
joshualholland <joshualholland@gmail.com>
2018-10-21 17:08:57 -0500

    random color function working

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-    $('#btnSubmit').click(function (e) {
-        e.preventDefault();
-        let text = $('input[type=text]').val();
-        $('div').append(`<h2>${text}</h2>`);
-        $('h2').mouseover(function () {
-            $('h2').css({ 'backgroundColor': 'blue', 'borderRadius': '15px' });
-        });
-    });
-    $('body').append('<div></div>');
     $('input[type=text]').keyup(function () {
         if ($(this).val() == "") {
             $('#btnSubmit').prop('disabled', true);
@@ -15,5 +6,21 @@ $(document).ready(function () {
             $('#btnSubmit').prop('disabled', false);
         };
     });
+    $('body').append('<ul></ul>');
+
+    $('#btnSubmit').click(function (e) {
+        e.preventDefault();
+        let text = $('input[type=text]').val();
+        $('ul').append(`<li>${text}</li>`);
+    });
+// Make sure each li can be selected without selecting all li and choose random color
+    $('ul').click(function (event) {
+        $(event.target).css({'color': getRandomColor()});
+    });
+// get random color for li
+    function getRandomColor() {
+        let randomColor = 'rgb(' + Math.floor((Math.random() * 256)) + ',' + Math.floor((Math.random() * 256)) + ',' + Math.floor((Math.random() * 256)) + ')';
+        return randomColor;
+    };
 });
 
